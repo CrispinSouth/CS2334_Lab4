d223fa763f4ee21c893db293d0d572a8df139f85
CrispinSouth <47156832+CrispinSouth@users.noreply.github.com>
2019-02-12 23:49:36 -0600

    Update Todo.java

--- Todo.java
+++ Todo.java
@@ -1,18 +1,24 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 13
+Last Modified_______: 2019-02-12
+OU ID_______________: 113358570
+OU EMAIL____________: crispin@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. complete methods in animal
+2. complete test animal
+3. complete  methods in equipment test
+4. complete methods in Zoo Test
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 10
+2. 10
+3. 20
+4. 20
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 10
+2. 15
+3. 30
+4. 60
 */}
