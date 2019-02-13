a411b1c86b9d6b9860e3019e22e73ce47c6d5b19
CrispinSouth <47156832+CrispinSouth@users.noreply.github.com>
2019-02-12 23:07:18 -0600

    Update AnimalTest.java

--- AnimalTest.java
+++ AnimalTest.java
@@ -35,19 +35,43 @@ public class AnimalTest
     		Assert.assertEquals(5.0, animal.getHeight());
     }
     
-    public void getColorTest() throws assertException {
+    public void getColorTest() throws AssertException {
     		String expected = "Blue";
+    		Animal animal = new Animal("Blue", "fish", 1.1, 5.0);
+    		
+    		String actual = animal.getColor();
+    		Assert.assertTrue(expected.equals(actual));
     }
     
-    public void getNameTest() throws assertException {
+    public void getNameTest() throws AssertException {
     		String expected = "fish";
+    		Animal animal = new Animal("Blue", "fish", 1.1, 5.0);
+    		
+    		String actual = animal.getName();
+    		Assert.assertTrue(expected.equals(actual));
     }
     
-    public void getWeightTest() throws assertException {
+    public void getWeightTest() throws AssertException {
     		double expected = 1.1;
+    		Animal animal = new Animal("Blue", "fish", 1.1, 5.0);
+    		
+    		double actual = animal.getWeight();
+    		Assert.assertEquals(expected, actual);
     }
     
-    public void getHeightTest() throws assertException {
+    public void getHeightTest() throws AssertException {
     		double expected = 5.0;
+    		Animal animal = new Animal("Blue", "fish", 1.1, 5.0);
+    		
+    		double actual = animal.getHeight();
+    		Assert.assertEquals(expected, actual);
+    }
+    
+    public void toStringTest() throws AssertException {
+    		String expected = "fish, a Blue-colored animal. 1.1 pounds, 5.0 inches\\\\n\\";
+    		Animal animal = new Animal("Blue", "fish", 1.1, 5.0);
+    		
+    		String actual = animal.toString();
+    		Assert.assertTrue(expected.equals(actual));
     }
 }
