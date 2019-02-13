53feb3c29745a6ce5d6cfc5ee47348961f6eaca5
CrispinSouth <47156832+CrispinSouth@users.noreply.github.com>
2019-02-12 22:57:28 -0600

    Update AnimalTest.java

--- AnimalTest.java
+++ AnimalTest.java
@@ -23,4 +23,31 @@ public class AnimalTest
     }
 
     // TODO: test full constructor, getters, and toString
+    public void fullConstructorTest() throws AssertException {
+    		Animal animal = new Animal("Blue", "fish", 1.1, 5.0);
+    		
+    		String expectedName = "fish";
+    		String expectedColor = "Blue";
+    		
+    		Assert.assertTrue(expectedColor.equals("Blue"));
+    		Assert.assertTrue(expectedName.equals("fish"));
+    		Assert.assertEquals(1.1, animal.getWeight());
+    		Assert.assertEquals(5.0, animal.getHeight());
+    }
+    
+    public void getColorTest() throws assertException {
+    		String expected = "Blue";
+    }
+    
+    public void getNameTest() throws assertException {
+    		String expected = "fish";
+    }
+    
+    public void getWeightTest() throws assertException {
+    		double expected = 1.1;
+    }
+    
+    public void getHeightTest() throws assertException {
+    		double expected = 5.0;
+    }
 }
